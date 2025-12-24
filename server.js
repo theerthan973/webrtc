ced5e7cdfac91189998ebc05716641fcc8552502
theerthan973 <73023530+theerthan973@users.noreply.github.com>
2025-12-24 20:13:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,69 +1,41 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
+app.use(express.static("public"));
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("*", (_, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-let webClient = null;
-let androidClient = null;
+const webClients = new Set();
+const androidClients = new Set();
 
 io.on("connection", socket => {
-  console.log("Connected:", socket.id);
   socket.emit("id", socket.id);
 
   socket.on("identify", type => {
-    socket.clientType = type;
+    if (type === "web") webClients.add(socket.id);
+    if (type === "android") androidClients.add(socket.id);
 
-    if (type === "web") {
-      webClient = socket.id;
-      if (androidClient) {
-        io.to(webClient).emit("android-client-ready", androidClient);
-        io.to(androidClient).emit("web-client-ready", webClient);
-      }
-    }
+    webClients.forEach(w => androidClients.forEach(a => {
+      io.to(w).emit("android-client-ready", a);
+      io.to(a).emit("web-client-ready", w);
+    }));
+  });
 
-    if (type === "android") {
-      androidClient = socket.id;
-      if (webClient) {
-        io.to(androidClient).emit("web-client-ready", webClient);
-        io.to(webClient).emit("android-client-ready", androidClient);
-      }
-    }
+  socket.on("control", data => {
+    io.to(data.to).emit("control", data);
   });
 
   socket.on("signal", data => {
-    if (data.to) {
-      io.to(data.to).emit("signal", data);
-    }
+    io.to(data.to).emit("signal", data);
   });
 
   socket.on("disconnect", () => {
-    console.log("Disconnected:", socket.id);
-
-    if (socket.id === androidClient) {
-      androidClient = null;
-      if (webClient) io.to(webClient).emit("android-client-disconnected");
-    }
-
-    if (socket.id === webClient) {
-      webClient = null;
-    }
+    webClients.delete(socket.id);
+    androidClients.delete(socket.id);
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () =>
-  console.log("Signaling server running on", PORT)
-);
+server.listen(3000, () => console.log("Server running"));
